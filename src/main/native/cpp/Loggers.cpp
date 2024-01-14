e7504ea2e01a084473935081d1fcb6c431b9b6cb
Isaac Turner <spacey_sooty@outlook.com>
2024-01-14 19:49:52 +0800

    [vendor core] begin work on loggers class

---
+++ src/main/native/cpp/Loggers.cpp
@@ -0,0 +1,46 @@
+// Copyright (c) 2023-2024 CurtinFRC
+// Open Source Software, you can modify it according to the terms
+// of the MIT License at the root of this project
+
+#include "Loggers.h"
+
+#include <networktables/NetworkTableValue.h>
+
+using namespace loggers;
+
+NTLogger::NTLogger(std::string name, std::string path,
+                   std::vector<NT_Table> tables)
+    : name(name), path(path), tables(tables) {}
+
+NTLogger::~NTLogger() {
+  for (std::thread* thread : this->threads) {
+    thread->join();
+  }
+}
+
+std::string NTLogger::GetName() {
+  return this->name;
+}
+
+std::string NTLogger::GetPath() {
+  return this->name;
+}
+
+std::vector<NT_Table> NTLogger::GetTables() {
+  return this->tables;
+}
+
+std::vector<std::thread*> NTLogger::GetThreads() {
+  return this->threads;
+}
+
+void NTLogger::OnLogTick() {
+  for (NT_Table table : this->tables) {
+    std::thread thread = std::thread([table]() {
+      for (std::string key : table->first.GetKeys()) {
+        table->first.GetEntry(key).SetDefaultValue((table->second));
+      }
+    });
+    threads.emplace_back(&thread);
+  }
+}
